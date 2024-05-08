eed6e9693ba73805332e9c2c913e1909c8614428
Vladislav Ulynets <vladislav.ulynets@outlook.com>
2024-05-08 14:29:32 +0300

    Changed design for main page and sign In/sign Up

--- frontend/src/components/signIn/SignIn.jsx
+++ frontend/src/components/signIn/SignIn.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './SignIn.scss';
@@ -7,20 +7,20 @@ import email_icon from '../assets/icons8-email-32.png';
 import password_icon from '../assets/icons8-password-32.png';
 
 const SignIn = () => {
-    const navigate = useNavigate();
+	const navigate = useNavigate();
 
-    const redirectToRegister = () => {
-        navigate('/register');
-      };
-    
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+	const redirectToRegister = () => {
+		navigate('/register');
+	};
 
-    const handleSignIn = async () => {
-        const data = {
-            email: email,
-            password: password
-        };
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleSignIn = async () => {
+		const data = {
+			email: email,
+			password: password,
+		};
 
 		axios
 			.post('http://localhost:8080/login', data, {
@@ -35,34 +35,42 @@ const SignIn = () => {
 			.catch((error) => {
 				console.log(error);
 			});
-    };
+	};
 
-    return (
-        <div className='container'>
-            <div className='header'>
-                <div className='text'>Sign In</div>
-                <div className='underline'></div>
-            </div>
-            <div className='inputs'>
-                <div className='input'>
-                    <img src={email_icon} alt='Email' />
-                    <input type='email' placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className='input'>
-                    <img src={password_icon} alt='Password' />
-                    <input type='password' placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
-                </div>
-            </div>
-            <div className="submit-container">
-                <div className="submit" onClick={() => handleSignIn()}>
-                    Sign In
-                </div>
-                <div className="submit gray" onClick={redirectToRegister}>
-                    Sign Up
-                </div>
-            </div>
-        </div>
-    );
+	return (
+		<div className="container">
+			<div className="header">
+				<div className="text">Sign In</div>
+				<div className="underline"></div>
+			</div>
+			<div className="inputs">
+				<div className="input">
+					<img src={email_icon} alt="Email" />
+					<input
+						type="email"
+						placeholder="Email"
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</div>
+				<div className="input">
+					<img src={password_icon} alt="Password" />
+					<input
+						type="password"
+						placeholder="Password"
+						onChange={(e) => setPassword(e.target.value)}
+					/>
+				</div>
+			</div>
+			<div className="submit-container">
+				<div className="submit" onClick={() => handleSignIn()}>
+					Sign In
+				</div>
+				<div className="submit gray" onClick={redirectToRegister}>
+					Sign Up
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SignIn;
